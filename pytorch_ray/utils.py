539ae00a2de34c455351569d0b5196c914e56b0d
Vince Jankovics <vincejankovics@gmail.com>
2019-09-15 15:14:51 +0100

    Utils update

--- pytorch_ray/utils.py
+++ pytorch_ray/utils.py
@@ -21,6 +21,8 @@ def to_numpy(input_arr):
         return input_arr.values
     elif isinstance(input_arr, np.ndarray):
         return input_arr
+    elif isinstance(input_arr, list):
+        return np.array(input_arr)
     else:
         raise ValueError("Cannot convert %s to Numpy" % (type(input_arr)))
 
@@ -120,26 +122,3 @@ class TimerStat:
         self._start_time = None
         self._total_time = 0.0
         self.count = 0
-
-
-class AverageMeter:
-    """Computes and stores the average and current value."""
-
-    def __init__(self):
-        self.val = None
-        self.avg = None
-        self.sum = None
-        self.count = None
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
