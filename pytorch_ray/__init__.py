18f4dbca9e47fa2597d7719f7fd2f0dbb46cee2f
Vince Jankovics <vincejankovics@gmail.com>
2019-09-17 16:16:22 +0100

    Logger refactor

--- pytorch_ray/__init__.py
+++ pytorch_ray/__init__.py
@@ -1,3 +1,4 @@
-from .pytorch_trainable import PyTorchRunner, PyTorchTrainable, logger_creator
+from .pytorch_trainable import PyTorchRunner, PyTorchTrainable
+from .logger import logger_creator
 
 __all__ = ["PyTorchTrainable", 'PyTorchRunner', 'logger_creator']
---
+++ pytorch_ray/logger.py
@@ -0,0 +1,42 @@
+from os import path as osp
+
+from ray.tune.logger import Logger
+from ray.tune.result import TIMESTEPS_TOTAL, TRAINING_ITERATION
+from torch.utils.tensorboard import SummaryWriter
+
+
+def logger_creator(config):
+    return PRLogger(config, config['logdir'])
+
+
+class PRLogger(Logger):
+    def _init(self):
+        purge_step = self.config.get('start_from', 0)
+
+        self._tb_writer = SummaryWriter(
+            log_dir=osp.join(self.logdir, 'logs'), purge_step=purge_step)
+
+        self._log_hparams()
+
+    def on_result(self, result):
+        step = result.get(TIMESTEPS_TOTAL) or result[TRAINING_ITERATION]
+
+        tmp = result.copy()
+
+        log_types = ['scalar', 'histogram', 'image', 'figure', 'text']
+        for phase in ['tng', 'val']:
+            metrics = tmp.get(phase, {})
+            for t in log_types:
+                for k, v in metrics.get(t, {}).items():
+                    log_fcn = getattr(self._tb_writer, f'add_{t}')
+                    log_fcn(k, v, global_step=step)
+
+        self._tb_writer.flush()
+
+    def _log_hparams(self):
+        if self.trial and self.trial.evaluated_params:
+            self._tb_writer.add_hparams(
+                hparam_dict=self.trial.evaluated_params)
+
+    def add_graph(self, model, inputs):
+        self._tb_writer.add_graph(model, inputs)
