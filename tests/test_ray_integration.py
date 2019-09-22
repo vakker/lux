fa69d5699f0671c444c646e1838a617086574723
Vince Jankovics <vincejankovics@gmail.com>
2019-09-22 14:01:11 +0100

    Test refactor

---
+++ tests/test_ray_integration.py
@@ -0,0 +1,40 @@
+from os import path as osp
+
+import pytest
+from ray import tune
+
+from pytorch_ray import PyTorchTrainable
+from pytorch_ray.logger import pr_logger_creator
+
+from .utils import create_runner, get_chkp_dir, trial_str_creator
+
+
+@pytest.mark.parametrize("num_gpus", [0, 1])
+def test_tune_train(start_ray, trainable_config, num_gpus):
+
+    trainable_config.update({
+        "num_gpus": num_gpus,
+        "num_cpus": 2,
+    })
+
+    trainable_config['runner_config']['hparams']['lr'] = tune.grid_search(
+        [1e-5, 1e-4])
+    trainable_config['runner_config']['hparams'][
+        'momentum'] = tune.grid_search([0, 0.9])
+
+    analysis = tune.run(
+        PyTorchTrainable,
+        loggers=[pr_logger_creator],
+        num_samples=1,
+        config=trainable_config,
+        trial_name_creator=trial_str_creator,
+        stop={"training_iteration": 5},
+        local_dir='./logs',
+        verbose=1)
+
+    # checks loss decreasing for every trials
+    for path, df in analysis.trial_dataframes.items():
+        tng_loss1 = df.loc[0, "scalar/tng/loss"]
+        tng_loss2 = df.loc[1, "scalar/tng/loss"]
+
+        assert tng_loss2 < tng_loss1
--- tests/test_trainable.py
+++ tests/test_trainable.py
@@ -85,34 +85,3 @@ def test_trainable_val_freq(trainable, val_freq):
             assert 'val/loss' in stats
         else:
             assert 'val/loss' not in stats
-
-
-@pytest.mark.parametrize("num_gpus", [0, 1])
-def test_tune_train(start_ray, trainable_config, num_gpus):
-
-    trainable_config.update({
-        "num_gpus": num_gpus,
-        "num_cpus": 2,
-    })
-
-    trainable_config['runner_config']['hparams']['lr'] = tune.grid_search(
-        [1e-5, 1e-4])
-    trainable_config['runner_config']['hparams'][
-        'momentum'] = tune.grid_search([0, 0.9])
-
-    analysis = tune.run(
-        PyTorchTrainable,
-        loggers=[pr_logger_creator],
-        num_samples=1,
-        config=trainable_config,
-        trial_name_creator=trial_str_creator,
-        stop={"training_iteration": 5},
-        local_dir='./logs',
-        verbose=1)
-
-    # checks loss decreasing for every trials
-    for path, df in analysis.trial_dataframes.items():
-        tng_loss1 = df.loc[0, "scalar/tng/loss"]
-        tng_loss2 = df.loc[1, "scalar/tng/loss"]
-
-        assert tng_loss2 < tng_loss1
