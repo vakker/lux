c724f2f961cc39ea085039cfb56f317586086fc8
Vince Jankovics <vincejankovics@gmail.com>
2020-04-30 17:14:05 +0100

    Rename folder

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='pytorch-ray',
+    name='lux',
     version='0.1',
     packages=find_packages(),
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     install_requires=[
         'torch>=1.2.0',
         'ray>=0.7.0',
